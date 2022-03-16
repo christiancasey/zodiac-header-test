9c0c4815a0db67fbee0c4e4c9e4e0ca662614641
christiancasey <christiancasey86@gmail.com>
2022-03-16 15:33:27 +0100

    First

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
 import logo from './logo.svg';
+import zodiac from './zodiac.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="header-container">
+          <img src={zodiac} className="App-logo" alt="logo" />
+        </div>
+        
+        <h1 className="zodiac-logotype">Zodiac</h1>
       </header>
     </div>
   );
